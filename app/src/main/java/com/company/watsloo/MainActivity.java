4a8124fad55d632a31e8a7cd5f439ff93cf3efc2
Zishuo Xu <xuzishuo1996@gmail.com>
2021-06-30 18:10:54 -0400

    test add simple data

--- app/src/main/java/com/company/watsloo/MainActivity.java
+++ app/src/main/java/com/company/watsloo/MainActivity.java
@@ -3,12 +3,48 @@ package com.company.watsloo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 public class MainActivity extends AppCompatActivity {
+
+    FirebaseDatabase database;
+    DatabaseReference reference;
+    FirebaseStorage firebaseStorage;
+    StorageReference storageReference;
+
+    private Button buttonUpload;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        database = FirebaseDatabase.getInstance();
+        reference = database.getReference();
+        firebaseStorage = FirebaseStorage.getInstance();
+        storageReference = firebaseStorage.getReference();
+
+        buttonUpload = findViewById(R.id.button_upload);
+
+        buttonUpload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                reference.child("item").setValue(1).addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void unused) {
+                        Toast.makeText(MainActivity.this,
+                                "Successfully add data!", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
     }
 }
