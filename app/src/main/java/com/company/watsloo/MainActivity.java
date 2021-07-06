9332fa21496075595af1c8e36f26e3f86aafe67a
y5mei <meiyaowen@gmail.com>
2021-07-06 16:03:47 -0400

    Takepicture (#2)
    * Read GPS information from a EXIF picture
    
    Require MediaStore Location permission to get the GPS information from the EXIF picture
    
    * Slide Bar added
    
    * Update MainActivity.java

--- app/src/main/java/com/company/watsloo/MainActivity.java
+++ app/src/main/java/com/company/watsloo/MainActivity.java
@@ -1,7 +1,13 @@
 package com.company.watsloo;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+
+import android.content.Context;
 import android.annotation.SuppressLint;
 import android.content.res.Resources;
 import android.graphics.Bitmap;
@@ -22,18 +28,79 @@ import java.util.ArrayList;
 import java.util.List;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.navigation.NavigationView;
+
+import org.jetbrains.annotations.NotNull;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final String PROJ_PATH = System.getProperty("user.dir");
 
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
+    private ActionBarDrawerToggle actionBarDrawerToggle;
+    private static final String PROJ_PATH = System.getProperty("user.dir");
     private Button buttonUpload;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Context thiscontext = this;
+
+        drawerLayout = findViewById(R.id.drawerLayout);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.open,R.string.close);
+        navigationView = findViewById(R.id.nav_view);
+
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        //https://www.legendblogs.com/navigation-drawer-layout-with-an-event-listener
+        // Add listener to each of the menu items
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull @NotNull MenuItem item) {
+                // Handle navigation view item clicks here.
+                int id = item.getItemId();
+
+                if (id == R.id.nav_overview) {
+                    drawerLayout.closeDrawer(GravityCompat.START);
+                    // send an intent to active the overview mode here
+                } else if (id == R.id.nav_follow) {
+                    // send an intent to active the following mode here
+                    drawerLayout.closeDrawer(GravityCompat.START);
+                } else if (id == R.id.nav_upload) {
+                    // send an intent to active the story upload mode here
+                    Toast.makeText(thiscontext, "upload your own story", Toast.LENGTH_LONG).show();
+                    drawerLayout.closeDrawer(GravityCompat.START);
+                    Intent intent = new Intent(thiscontext, UploadNewPlaceActivity.class );
+                    startActivity(intent);
+
+                }
+
+                return true;
+            }
+        });
+
+
+    }
+
+    // Display the sidebar when the toggle is pressed
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        Toast.makeText(this, "Welcome to explore our Campus", Toast.LENGTH_LONG).show();
+        if (actionBarDrawerToggle.onOptionsItemSelected(item)){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 
         // for firebase test, ignore
         buttonUpload = findViewById(R.id.button_upload);
@@ -63,7 +130,9 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
-    }
+
+
+
 
     public void gotoUploadNewPlaceActivity(View view){
         Intent intent = new Intent(this, UploadNewPlaceActivity.class );
