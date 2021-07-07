6a5f7edc85627c51bb3c002890ab13a9471601ef
super56 <m18810429379@163.com>
2021-07-07 20:39:56 +0800

    Optimize xml and read function
    add the fragment of story list that displays pictures and texts. optimize the layout of detail page.

--- app/src/main/java/com/company/watsloo/DetailActivity.java
+++ app/src/main/java/com/company/watsloo/DetailActivity.java
@@ -1,11 +1,13 @@
 package com.company.watsloo;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -16,6 +18,8 @@ import java.util.ArrayList;
 public class DetailActivity extends AppCompatActivity {
 
     private TextView story_detail;
+    private TextView story_title;
+    private ImageView story_image;
 
 
     @Override
@@ -23,16 +27,17 @@ public class DetailActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
         story_detail= findViewById(R.id.detailText);
-        //Intent intent=getIntent();
-        String input="input";
-        story_detail.setText(input);
+        story_title = findViewById(R.id.titleText);
+        story_image = findViewById(R.id.storyImage);
 
-
-
-
-
-
-
+        Bundle bundle = getIntent().getExtras();
+        String spot_title_str = bundle.getString("spot_title");
+        String story_title_str = bundle.getString("story_title");
+        story_title.setText(story_title_str);
+        story_detail.setText("    inputaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\n" +
+                "    dddfd");
+        story_title.setTextSize(20);
+        story_image.setImageResource(R.drawable.uwaterloologo);
 
 
 
@@ -40,9 +45,6 @@ public class DetailActivity extends AppCompatActivity {
 
 
     }
-
-
-
 
 
 
--- app/src/main/java/com/company/watsloo/SpotActivity.java
+++ app/src/main/java/com/company/watsloo/SpotActivity.java
@@ -13,10 +13,12 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.HashMap;
 
 public class SpotActivity extends AppCompatActivity {
 
@@ -25,54 +27,88 @@ public class SpotActivity extends AppCompatActivity {
     private ListView listView;
     ArrayList<String> story_list = new ArrayList<>();
 
+    ArrayList<HashMap<String, Object>> dataList = new ArrayList<HashMap<String, Object>>();
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_spot);
-        spot_title= findViewById(R.id.spotTitleText);
-        Intent intent=getIntent();
-        String title=intent.getStringExtra("title");
+        spot_title = findViewById(R.id.spotTitleText);
+        Intent intent = getIntent();
+        String title = intent.getStringExtra("title");
         spot_title.setText(title);
 
-        // spot stories
-//        spot_content = findViewById(R.id.spotContent);
-//        String content = "Story1' tilte\nStory2's title\ntest";
-//        spot_content.setText(content);
-//        spot_content.setMovementMethod(ScrollingMovementMethod.getInstance());
+        int[] image_story = new int[]{R.drawable.uwaterloologo, R.drawable.uwaterloologo};
+        String[] title_story = new String[image_story.length];
+        switch (title) {
+            case "Mathematics & Computer Building":
+                title_story[0] = "story11";
+                title_story[1] = "story22";
+                for (int i = 0; i < image_story.length; i++) {
 
+                    HashMap<String, Object> map = new HashMap<String, Object>();
+                    map.put("text", title_story[i]);
+                    map.put("pic", image_story[i]);
 
-        story_list.add("story1");
-        story_list.add("story2"); // 后续取数据库数据
+                    dataList.add(map);
+                }
+                break;
+            case "Marker in Loo":
+                title_story[0] = "story33";
+                title_story[1] = "story44";
+                for (int i = 0; i < image_story.length; i++) {
+                    HashMap<String, Object> map = new HashMap<String, Object>();
+                    map.put("text", title_story[i]);
+                    map.put("pic", image_story[i]);
+                    dataList.add(map);
+                }
+                break;
+
+        }
+
         listView = findViewById(R.id.spotList);
-        ArrayAdapter<String> adapter = new ArrayAdapter( this,R.layout.support_simple_spinner_dropdown_item,story_list);
+        SimpleAdapter adapter = new SimpleAdapter(this, dataList,
+                R.layout.fragment_story_list, new String[]{"text", "pic"},
+                new int[]{R.id.story_title, R.id.story_image});
+
         listView.setAdapter(adapter);
 
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+//        story_list.add("story1");
+//        story_list.add("story2");
+//        listView = findViewById(R.id.spotList);
+//        ArrayAdapter<String> adapter = new ArrayAdapter( this,R.layout.support_simple_spinner_dropdown_item,story_list);
+//        listView.setAdapter(adapter);
+
+        // story's detail -> DetailActivity
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 
             @Override
-            public  void  onItemClick(AdapterView<?> adapterView, View view, int i, long l)
-            {
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
                 Intent intentDetail = new Intent();
                 intentDetail.setClass(SpotActivity.this, DetailActivity.class);
+                Bundle bundle = new Bundle();
+                bundle.putString("story_title", title_story[position]);
+                bundle.putString("spot_title", title);
+
+                intentDetail.putExtras(bundle);
                 startActivity(intentDetail);
             }
-
         });
 
 
-        //button :submit story/take pictures->文哥 -> 我用 intent link 过去了（梅耀文留)
-        Button buttonSubmit = (Button)this.findViewById(R.id.buttonSubmit);
+        //button :submit story/take pictures
+        Button buttonSubmit = (Button) this.findViewById(R.id.buttonSubmit);
         buttonSubmit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                gotoUploadNewPlaceActivity(null);
-//                Intent intentSubmit = new Intent();
-//                intentSubmit.setClass(SpotActivity.this, TakePicturesActivity.class);
-//                startActivity(intentSubmit);
-                }
+                Intent intentSubmit = new Intent();
+                intentSubmit.setClass(SpotActivity.this, TakePicturesActivity.class);
+                startActivity(intentSubmit);
+            }
         });
         //back to home page
-        Button buttonBack = (Button)this.findViewById(R.id.buttonBack);
+        Button buttonBack = (Button) this.findViewById(R.id.buttonBack);
         buttonBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -83,14 +119,4 @@ public class SpotActivity extends AppCompatActivity {
             }
         });
     }
-
-
-    public void gotoUploadNewPlaceActivity(View view){
-        Intent intent = new Intent(this, UploadNewPlaceActivity.class );
-        startActivity(intent);
-    }
-
-
-
-
 }
