2c36a4a655f7ae1ffb83b103982ee488e1c7b8a3
Zishuo Xu <xuzishuo1996@gmail.com>
2021-08-01 23:45:59 -0400

    remove redundant code

--- app/src/main/java/com/company/watsloo/data/DataOperation.java
+++ app/src/main/java/com/company/watsloo/data/DataOperation.java
@@ -31,53 +31,6 @@ public class DataOperation {
     private static DatabaseReference dbRef = FirebaseDatabase.getInstance().getReference();
     private static StorageReference storageRef = FirebaseStorage.getInstance().getReference();
 
-//    public static void addItem(Context context, Item item) {
-//        DatabaseReference itemRef = dbRef.child(item.getName());
-//        itemRef.child("latitude").setValue(item.getLatitude()).addOnSuccessListener(new OnSuccessListener<Void>() {
-//            @Override
-//            public void onSuccess(Void unused) {
-//                Toast.makeText(context,
-//                        "Successfully add latitude!", Toast.LENGTH_SHORT).show();
-//            }
-//        }).addOnFailureListener(new OnFailureListener() {
-//            @Override
-//            public void onFailure(@NonNull Exception e) {
-//                Toast.makeText(context,
-//                        "Fail to add latitude!", Toast.LENGTH_SHORT).show();
-//            }
-//        });
-//        itemRef.child("longitude").setValue(item.getLongitude()).addOnSuccessListener(new OnSuccessListener<Void>() {
-//            @Override
-//            public void onSuccess(Void unused) {
-//                Toast.makeText(context,
-//                        "Successfully add longitude!", Toast.LENGTH_SHORT).show();
-//            }
-//        }).addOnFailureListener(new OnFailureListener() {
-//            @Override
-//            public void onFailure(@NonNull Exception e) {
-//                Toast.makeText(context,
-//                        "Fail to add longitude!", Toast.LENGTH_SHORT).show();
-//            }
-//        });
-//        itemRef.child("description").setValue(item.getDescription()).addOnSuccessListener(new OnSuccessListener<Void>() {
-//            @Override
-//            public void onSuccess(Void unused) {
-//                Toast.makeText(context,
-//                        "Successfully add description!", Toast.LENGTH_SHORT).show();
-//            }
-//        }).addOnFailureListener(new OnFailureListener() {
-//            @Override
-//            public void onFailure(@NonNull Exception e) {
-//                Toast.makeText(context,
-//                        "Fail to add description!", Toast.LENGTH_SHORT).show();
-//            }
-//        });
-//
-//        if (!item.getStories().isEmpty()) {
-//            addStories(context, item.getName(), item.getStories());
-//        }
-//    }
-
     public static void addStories(Context context, String itemName, List<String> stories) {
         DatabaseReference itemRef = dbRef.child(itemName);
         DatabaseReference storiesRef = itemRef.child("stories");
@@ -124,13 +77,8 @@ public class DataOperation {
             public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
                 // taskSnapshot.getMetadata() contains file metadata such as size, content-type, and download URL.
                 String filepath = taskSnapshot.getMetadata().getPath();
-                dbRef.child(itemName).child("images").child(randomId.toString()).setValue(filepath).addOnSuccessListener(new OnSuccessListener<Void>() {
-                    @Override
-                    public void onSuccess(Void unused) {
-                        Toast.makeText(context,
-                                "Successfully store the image!", Toast.LENGTH_SHORT).show();
-                    }
-                }).addOnFailureListener(new OnFailureListener() {
+                dbRef.child(itemName).child("images").child(randomId.toString()).setValue(filepath)
+                        .addOnFailureListener(new OnFailureListener() {
                     @Override
                     public void onFailure(@NonNull Exception e) {
                         Toast.makeText(context,
